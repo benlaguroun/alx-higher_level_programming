88c0a79cc3eb3523a19779a5393a1ccf44508f80
benlaguroun <benlaguroun@gmail.com>
2023-08-10 16:54:34 -0700

    1-calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the sum, difference, product, and quotient of 10 and 5."""  # Updated comment
-    
-    # Importing functions directly from the module
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
     from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
 
-    # Printing the results using f-strings (Python 3.6+)
-    print(f"{a} + {b} = {add(a, b)}")  # Using f-string
-    print(f"{a} - {b} = {sub(a, b)}")
-    print(f"{a} * {b} = {mul(a, b)}")
-    print(f"{a} / {b} = {div(a, b)}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
+
