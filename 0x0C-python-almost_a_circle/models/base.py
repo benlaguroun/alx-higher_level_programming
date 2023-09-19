6ff94ddcfe249aaf9aa1bf499ec9699fd2df9321
benlaguroun <benlaguroun@gmail.com>
2023-09-19 22:35:39 +0100

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,11 +19,14 @@ class Base:
         '''Jsonifies a dictionary so it's quite rightly and longer'''
         if list_dictionaries is None:
             return '[]'
-        return json.dumps(list_dictionaries)
+        else:
+            return json.dumps(list_dictionaries)
 
     @staticmethod
     def from_json_string(json_string):
-    
+        '''
+            Returns a dict from a string
+        '''
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
