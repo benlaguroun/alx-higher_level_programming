a7eae74ad793db3c2f4ec85f048be210fda5a73f
benlaguroun <benlaguroun@gmail.com>
2023-11-15 10:23:17 -0800

    task100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 
-const { list } = require('./100-data.js');
-
-const newList = list.map((value, index) => value * index);
-
-console.log('Initial List:', list);
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));
