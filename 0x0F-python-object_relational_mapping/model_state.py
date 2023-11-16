9454dd6ff0513f6d7af4481035162478a77353ec
benlaguroun <benlaguroun@gmail.com>
2023-11-16 12:22:19 -0800

    task 0

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" Includes the State class definition and a declarative_base() instance named Base """
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+class State(Base):
+
+    """ Defines a class with attributes for representing the id and name of each state """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" Includes the State class definition and an instance named Base, created using declarative_base() """
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class State(Base):
+
+    """ Defines a class with attributes for representing the id and name of each state """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="states")
