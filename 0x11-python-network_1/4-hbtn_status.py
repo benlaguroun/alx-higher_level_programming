6c5458e49fbb168747bc2684e7f73b4d66eb8de6
benlaguroun <benlaguroun@gmail.com>
2023-12-02 10:36:37 -0800

    task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" Fetches the status of https://intranet.hbtn.io. """
+import requests
+if __name__ == "__main__":
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
